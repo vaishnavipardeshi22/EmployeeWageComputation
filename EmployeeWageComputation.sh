a636ec4cb643029ce404e9fdc3d922e88942e599
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-13 15:08:26 +0530

    REFACTOR : Replacing if_elif Statement by Case Statement

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -3,23 +3,26 @@
 echo " ****************************** WELCOME TO EMPLOYEE WAGE COMPUTATION ****************************** "
 
 #CONSTANT
-IS_PRESENT=1
+IS_PART_TIME=1
 IS_FULL_TIME=2
 EMP_WAGE_RATE_PER_HOUR=20
 
 #GENERATE RANDOM VALUE
 randomCheck=$(( RANDOM % 3 ))
 
-#GET EMPLOYEE HOURS 
-if [ $IS_FULL_TIME -eq $randomCheck ]
-then
-	employeeHrs=8
-elif [ $IS_PART_TIME -eq $randomCheck ]
-then
-	employeeHrs=4
-else
-	employeeHrs=0
-fi
+#CHECK CONDITION AND GET EMPLOYEE HOURS
+case $randomCheck in
+	$IS_FULL_TIME)
+		employeeHrs=8
+		;;
+	$IS_PART_TIME)
+		employeeHrs=4
+		;;
+	*)
+		employeeHrs=0
+		;;
+esac
 
 #CALCULATE DAILY WAGE FOR EMPLOYEE
 salary=$(( employeeHrs * EMP_WAGE_RATE_PER_HOUR ))
+
